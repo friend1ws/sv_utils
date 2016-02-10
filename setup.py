32a01aa50915de73099c40db5c600bdf199825fd
friend1ws <friend1ws@gmail.com>
2016-02-10 15:52:28 +0900

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='sv_utils',
       version='0.3.0',
-      description='Python tools for analyzing GenomonSV results',
+      description='Python programs for analyzing GenomonSV results',
       author='Yuichi Shiraishi',
       author_email='friend1ws@gamil.com',
       url='https://github.com/friend1ws/sv_utils',
