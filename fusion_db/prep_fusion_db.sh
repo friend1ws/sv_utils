86c39a5807e39f78dcc29726389b1c7e53601f70
friend1ws <friend1ws@gmail.com>
2018-01-10 21:46:56 +0900

    deleted fusion_db files

--- fusion_db/prep_fusion_db.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-python prep_fusion_db.py > ../sv_utils/data/fusion_db.txt
--- fusion_db/target2type/get_target2type.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-python get_target2type.py target_gene.txt target2type.txt 
-
-
