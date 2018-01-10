8c368ff1730621e194e347ee88498f17e4b7eb77
friend1ws <friend1ws@gmail.com>
2018-01-10 10:30:03 +0900

    add test_count.py file and its data

---
+++ tests/test_count.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+import unittest
+import os, glob, tempfile, shutil, filecmp
+import sv_utils
+
+class TestCount(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = sv_utils.parser.create_parser()
+
+    def test1(self):
+
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+        tmp_dir = tempfile.mkdtemp()
+
+        all_sv_file = glob.glob(cur_dir + "/data/*.genomonSV.result.txt")
+        sample2type = {}
+        with open(cur_dir + "/data/sample_type.txt", 'r') as hin:
+            for line in hin:
+                F = line.rstrip('\n').split('\t')
+                sample2type[F[0]] = F[1]
+    
+        with open(tmp_dir + "/CCLE.genomonSV.reulst_list.txt", 'w') as hout:
+            for sv_file in sorted(all_sv_file):
+                sample = os.path.basename(sv_file).replace(".genomonSV.result.txt", '')
+                print >> hout, "%s\t%s\t%s" % (sample, sample2type[sample], sv_file) 
+
+        result_lsit_file = tmp_dir + "/CCLE.genomonSV.reulst_list.txt"
+        output_file = tmp_dir + "/CCLE.genomonSV.result.count.txt"
+        answer_file = cur_dir + "/data/count/CCLE.genomonSV.result.count.txt"
+ 
+        args = self.parser.parse_args(["count", result_lsit_file, output_file])
+        args.func(args)
+
+        self.assertTrue(filecmp.cmp(output_file, answer_file, shallow=False))
+
+        shutil.rmtree(tmp_dir)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
