50badd6814e88ce5edb323d39dc336c65eba0639
friend1ws <friend1ws@gmail.com>
2018-01-28 15:47:45 +0900

    modify version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = 'sv_utils',
-    version = '0.5.0rc2',
+    version = '0.5.0',
     description='Python programs for analyzing GenomonSV results',
     url = 'https://github.com/friend1ws/sv_utils',
     author = 'Yuichi Shiraishi',
